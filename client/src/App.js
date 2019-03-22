65a15f886c87ceb644a808af9f42c41642e42016
Gurkirt Heerey <g_heerey@u.pacific.edu>
2019-03-22 09:19:36 -0700

    Grab user location

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './static/app.css';
 
-export default function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1>Welcome to BusMe</h1>
-      </header>
-    </div>
-  );
-};
+export default class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: null
+    };
+  }
+
+  findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({  location: position.coords });
+      }
+      // {
+      //   maximumAge:0,
+      //   enableHighAccuracy: true,
+      //   timeout: 5000
+      // }
+    );
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <header className="app-header">
+          <button onClick={ this.findCoordinates }>Press me to find location!</button>
+          <h1>Welcome to BusMe</h1>
+          <h2>Please input total number of passengers:</h2>
+          <input type="text"></input>
+          <button type="submit">Submit</button>
+
+        </header>
+      </div>
+    );
+  }
+}
