dd9fe01612f86d7d49a0d5782129dff3eac4ab36
Shane Duan <s_duan@u.pacific.edu>
2019-03-23 11:43:58 -0700

    Add extended AsyncNamespace with app context

---
+++ server/bus_me/endpoint/_async_namespace.py
@@ -0,0 +1,17 @@
+from socketio import AsyncNamespace
+
+
+class _AsyncNamespace(AsyncNamespace):
+    """
+    Extended `AsyncNamespace` that accepts a deployment object (eg
+    `aiohttp.web.Application`) in order to allow interactions with non socket
+    portions of the app.
+
+    Dumb hack courtesy of:
+    https://github.com/miguelgrinberg/python-socketio/issues/142
+    """
+
+    def __init__(self, app, namespace=None):
+        super().__init__(namespace=namespace)
+        self.app = app
+
