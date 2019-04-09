5f85e3bf0b7012d48110793f7270de021effbf95
Shane Duan <s_duan@u.pacific.edu>
2019-04-09 13:23:48 -0700

    Add create and subscribe to routes

--- server/bus_me/endpoint/admin.py
+++ server/bus_me/endpoint/admin.py
@@ -1,6 +1,7 @@
+from datetime import timedelta
 from uuid import uuid4
 
-from ..entities import db, Organization
+from ..entities import db, Location, Organization, Route, Stop, Timetable, User
 from ._require_auth import require_auth
 from .login import LoginNamespace
 
@@ -16,7 +17,8 @@ class AdminNamespace(LoginNamespace):
                 If present and exists in the database, it will be assumed that
                 that id should be updated. Otherwise, a new organization is
                 created.
-            values: dict: Values to update organization with.
+            values: dict:
+                Values to update organization with. (see `Organization`).
         
         returns: str:
             UUID of organization updated (may be different from sent ID).
@@ -24,7 +26,7 @@ class AdminNamespace(LoginNamespace):
 
         org = None
         if "id" in data:
-            org = await db.get_or_create(Organization, uuid=data["id"])
+            org = await db.get(Organization, uuid=data["id"])
         if not org:
             org = await db.create(Organization, uuid=uuid4())
 
@@ -33,3 +35,76 @@ class AdminNamespace(LoginNamespace):
         await db.update(org)
 
         return str(org.uuid)
+
+    @require_auth(permissions=["create_route"])
+    async def on_create_route(self, sid, _, data):
+        """
+        Create or update a route.
+
+        schema: dict:
+            id: int?:
+                Route ID to update or create (will only allow update of own
+                organization). TODO: implement organization restrictions.
+            name: str?: Name of the route.
+            stops: dict[]: per:
+                at: [float, float]: Geolocation, longitude and latitude.
+                time: float:
+                    Expected time from last stop (in seconds). First stop will
+                    be measured from last stop.
+
+        returns: int: ID of newly created route.
+        """
+        route = None
+        if "id" in data:
+            route = await db.get(Route, id=data["id"])
+            route.name = data.get("name", route.name)
+        if not route:
+            route = await db.create(Route, name=data.get("name", uuid4()))
+
+        stop_data = data["stops"]
+
+        # prepare stops and locations
+        locations = [
+            (await db.get_or_create(Location, long=long, lat=lat))[0]
+            for long, lat in (s["at"] for s in stop_data)
+        ]
+        stops = (
+            (stop, (await db.get_or_create(Stop, location=location))[0])
+            for location, stop in zip(locations, stop_data)
+        )
+
+        # get timetables from database
+        timetables = []
+        async for data, stop in stops:
+            timetable = await db.create(
+                Timetable,
+                route=route,
+                stop=stop,
+                expected_duration=timedelta(seconds=data["time"]),
+            )
+            timetables.append(timetable)
+
+        # link timetables
+        for i, timetable in enumerate(timetables):
+            try:
+                timetable.next_stop = timetables[i + 1]
+            except IndexError:
+                timetable.next_stop = timetables[0]
+            await db.update(timetable)
+
+        # link first timetable to route
+        route.timetable = timetables[0]
+        await db.update(route)
+        return route.id
+
+    @require_auth(permissions=["create_route"])
+    async def on_set_route_active(self, sid, _, route_id, active):
+        """
+        Set route as active or inactive. Ignores if invalid id.
+
+        schema: [int, bool]:
+            route_id, active: Set route with `route_id` as `active`.
+        """
+        route = await db.get(Route, id=route_id)
+        route.active = active
+        await db.update(route)
--- server/bus_me/endpoint/rider.py
+++ server/bus_me/endpoint/rider.py
@@ -1,8 +1,9 @@
-from datetime import datetime, timedelta
+from asyncio import sleep
+from datetime import timedelta
 
 from peewee import IntegrityError
 
-from ..entities import db, Location, User
+from ..entities import db, Location, Stop, UserLocation, User
 from ._require_auth import require_auth
 from .login import LoginNamespace
 
@@ -26,7 +27,8 @@ class RiderNamespace(LoginNamespace):
         """
         try:
             user, _ = await db.get_or_create(User, oidc_id=auth.user_id)
-            await db.create(Location, user=user, long=long, lat=lat)
+            user.location = await db.create(UserLocation, user=user, long=long, lat=lat)
+            await db.update(user)
             _log.info(f"User {auth.user_id} updated location to {(long, lat)}")
         except IntegrityError:
             await self.emit(
@@ -37,3 +39,57 @@ class RiderNamespace(LoginNamespace):
                 },
                 room=sid,
             )
+
+    @require_auth()
+    async def on_sub_stops(self, sid, auth, long_dist=0.01, lat_dist=0.01):
+        """
+        Start subscription to stops, once every 10 seconds. Yields to event
+        "stops".
+
+        schema: [float, float]: (default: (0.01, 0.01))
+            Degrees longitude and latitude from current location to search
+            within.
+        
+        yields: int[]: List of nearby stops by ID.
+        """
+
+        async def update_stops():
+            nonlocal self, sid, auth, long_dist, lat_dist
+            # get location
+
+            user = await db.get(User, oidc_id=auth.user_id)
+            if user:
+                with db.allow_sync():
+                    location = user.location
+            else:
+                location = None
+
+            if location is None:
+                _log.info(f"User {auth.user_id} does not have a location.")
+                return
+
+            long_min = location.long - long_dist
+            long_max = location.long + long_dist
+            lat_min = location.lat - lat_dist
+            lat_max = location.lat + lat_dist
+
+            with db.allow_sync():
+                stop_query = (
+                    Stop.select()
+                    .join(Location)
+                    .where(
+                        Location.long.between(long_min, long_max)
+                        & Location.lat.between(lat_min, lat_max)
+                    )
+                )
+                stops = [s.id for s in stop_query]
+
+            await self.emit("stops", stops, room=sid)
+
+        _log.info(f"User {auth.user_id} subscribed to stops.")
+        await update_stops()
+        while await sleep(10, result=True):
+            if sid not in self.active_users:
+                _log.info(f"User {auth.user_id} inactive, stopping stops updates.")
+                return
+            await update_stops()
