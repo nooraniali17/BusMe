f847819a7472b1509d2f87a5a8be396ad8d9f222
Shane Duan <s_duan@u.pacific.edu>
2019-04-10 16:51:38 -0700

    Use modern javascript features

--- js_client/main.js
+++ js_client/main.js
@@ -1,12 +1,9 @@
-var numInParty = 0;
-var myLocation, myLat, myLong;
-var map;
-var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-var infoWindow;
-
+let map;
+const image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+let infoWindow;
 
 function setNumInParty() {
-    numInParty = document.getElementById("numInParty").value
+    const numInParty = document.getElementById("numInParty").value;
     if (isNaN(numInParty)) {
         alert("Please enter a number.");
         return;
@@ -15,48 +12,40 @@ function setNumInParty() {
         alert("Please enter a number between 0 and 10.");
         return;
     }
-    url = `./passengerSubmission.html?numInParty=${encodeURIComponent(numInParty)}`;
-
-    document.location.href = url;
+    document.location.href = `./passengerSubmission.html?numInParty=${encodeURIComponent(numInParty)}`;
 }
 
-
 function initMap() {
-    var myMapCenter = { lat: 37.981161, lng: -121.312040 };
     infoWindow = new google.maps.InfoWindow();
 
     //Map loads this area first
     map = new google.maps.Map(document.getElementById('map'), {
-        center: myMapCenter,
+        center: { lat: 37.981161, lng: -121.312040 },
         zoom: 15,
         gestureHandling: 'greedy'
     });
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            var myLocation = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
+        navigator.geolocation.getCurrentPosition(position => {
+            const myLocation = {
+                lat: parseFloat(position.coords.latitude),
+                lng: parseFloat(position.coords.longitude)
             };
-            myLat = myLocation.lat;
-            myLong = myLocation.lng;
-            myLocation = { lat: parseFloat(myLat), lng: parseFloat(myLong) };
             infoWindow.setPosition(myLocation);
 
             infoWindow.setContent("Location Found");
             infoWindow.open(map);
 
-            var request = {
+            service = new google.maps.places.PlacesService(map);
+            service.textSearch({
                 location: myLocation,
                 radius: '50',
                 center: myLocation,
                 query: 'bus stops'
-            };
-            service = new google.maps.places.PlacesService(map);
-            service.textSearch(request, (results, status) => {
+            }, (results, status) => {
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
-                    for (var i = 0; i < results.length; i++) {
-                        createMarker(results[i]);
+                    for (const r of results) {
+                        createMarker(r);
                     }
                 }
             });
@@ -72,7 +61,7 @@ function initMap() {
 
 //EXPERIMENT WITH CREATING MARKERS
 function createMarker(place) {
-    var marker = new google.maps.Marker({
+    const marker = new google.maps.Marker({
         map: map,
         position: place.geometry.location,
         animation: google.maps.Animation.DROP
