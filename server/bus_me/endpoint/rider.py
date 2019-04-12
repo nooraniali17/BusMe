68c738d098f1abdbad98b94d8aa5973a1426056b
Shane Duan <s_duan@u.pacific.edu>
2019-04-12 09:31:09 -0700

    Split stop_data endpoint to DataNamespace

--- server/bus_me/endpoint/rider.py
+++ server/bus_me/endpoint/rider.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 from peewee import IntegrityError
 
-from ..entities import db, Location, Route, Stop, Timetable, UserLocation, User
+from ..entities import db, Location, Stop, UserLocation, User
 from ._require_auth import require_auth
 from .login import LoginNamespace
 
@@ -97,24 +97,3 @@ class RiderNamespace(LoginNamespace):
                 _log.info(f"User {auth.user_id} inactive, stopping stops updates.")
                 return
             await update_stops()
-
-    async def on_stop_data(self, sid, stop_id):
-        """
-        Get stop data.
-
-        schema: int: Stop ID, given by `sub_stops`.
-
-        returns: int[]: List of time tables at this stop by ID.
-        TODO: fill out return schema
-        """
-        tr_query = await db.prefetch(
-            Timetable.select().join(Stop).where(Stop.id == stop_id), Route.select()
-        )
-
-        return {
-            tr.id: {
-                "route": tr.route.id,
-                "expected_duration": tr.expected_duration.seconds,
-            }
-            for tr in tr_query
-        }
