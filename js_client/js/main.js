f07139cc37d307c656ef0bdff3b3e50937a09732
Shane Duan <s_duan@u.pacific.edu>
2019-04-12 17:25:06 -0700

    Fix party size input form
    - relies on browser input form checking
    - uses event target, not document query

--- js_client/js/main.js
+++ js_client/js/main.js
@@ -3,18 +3,12 @@ import { sleep } from "./es6-compat/util.js";
 import getMap, { updatePosition } from "./map.js";
 import socket from "./socket.js";
 
-window.setPartySize = async () => {
-  const party = document.getElementById('party').value;
-  if (isNaN(party)) {
-    alert('Please enter a number.');
-    return;
-  }
-  if (party < 1 || party >= 10) {
-    alert('Please enter a number between 0 and 10.');
-    return;
-  }
+window.setPartySize = async e => {
+  const party = Number(e.target[0].value);
 
   (await socket()).emitAsync("check_in", { party });
+
+  e.preventDefault();
   // document.location.href = `./submit.html?party=${encodeURIComponent(party)}`;
 }
 
