38e0346962ca2f811a3d78f021e4b95c13e732f7
Ali <a_noorani@u.pacific.edu>
2019-04-15 22:43:35 -0700

    added comments

--- express_server/app.js
+++ express_server/app.js
@@ -5,13 +5,15 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// setup cors
+// setup cors which allows clients and
+// servers to communicate when on different domains
 app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
+// credentials needed to connect to database on Kurt's machine
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
@@ -19,6 +21,7 @@ const connection = mysql.createConnection({
   database: "busme_db"
 });
 
+// function to check for errors
 connection.connect(function(err) {
   if (err) {
     return console.error("error: " + err.message);
@@ -26,22 +29,29 @@ connection.connect(function(err) {
   console.log("Connected to MySQL server!");
 });
 
+// retrieves information from the database
 app.get("/", (req, res) => {
   try {
+    // this sql code will grab all the data from the
+    // table in the database named pass_info
     const sql = "select * from pass_info";
 
+    // sends sql command to database and returns results
     connection.query(sql, function(err, results) {
       if (err) {
         return res.sendStatus(500);
       }
       return res.send(results);
     });
+    // catch block needed to elegantly exit program
+    // if something goes wrong
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
   }
 });
 
+// sends and saves information to the database
 app.post("/", (req, res) => {
   try {
     const { body } = req;
@@ -50,6 +60,8 @@ app.post("/", (req, res) => {
       return res.sendStatus(400);
     }
 
+    // creating sql command to put values in the
+    // correct columns of database
     const sql = `
       insert into pass_info
         (num_pass, latitude, longitude, stop_name)
@@ -59,6 +71,8 @@ app.post("/", (req, res) => {
     }')
     `;
 
+    // catch block needed to elegantly exit program
+    // if something goes wrong
     connection.query(sql, function(err, result) {
       if (err) {
         console.log("error " + err);
@@ -72,4 +86,5 @@ app.post("/", (req, res) => {
   }
 });
 
+// shows us connection status
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
