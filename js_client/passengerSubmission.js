4f1b1e8b26e34e8ee52a8db69190bf43821a83db
Gurkirt Heerey <g_heerey@u.pacific.edu>
2019-04-17 16:43:37 -0700

    Button accepts yes/no user input

--- js_client/passengerSubmission.js
+++ js_client/passengerSubmission.js
@@ -1,7 +1,6 @@
 let finalName;
 
 function initPage() {
-  // POSTING  tripInfo
   const tripInfo = localStorage.getItem("tripInfo");
   const parsedTripInfo = JSON.parse(tripInfo);
 
@@ -11,32 +10,42 @@ function initPage() {
 }
 
 function cancelRequest() {
-  const Url = "http://2abb7c15.ngrok.io";
-  const numInParty = document.getElementById("numInParty");
-  const finalName = document.getElementById("busStopLabel");
 
-  const Data = {
-    num_pass: numInParty,
-    stop_name: finalName
-  };
+  let c = confirm("Are you sure you wish to continue?") 
 
-  const payLoad = {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method: "GET"
-  };
+  if (c) {
+    
+    const Url = "http://2abb7c15.ngrok.io";
+    const numInParty = document.getElementById("numInParty");
+    const finalName = document.getElementById("busStopLabel");
+  
+    const Data = {
+      num_pass: numInParty,
+      stop_name: finalName
+    };
+  
+    const payLoad = {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "GET"
+    };
+  
+  
+    fetch(Url, payLoad)
+      .then(data => data.json())
+      .then(res => {
+  
+        console.log(res);
+      })
+      .catch(error => console.log(error));
 
+      alert("You've cancelled your request...");
+  }
 
-  fetch(Url, payLoad)
-    .then(data => data.json())
-    .then(res => {
-
-      console.log(res);
-    })
-    .catch(error => console.log(error));
-
-//  alert("HELLO WORLD");
+  else {
+    alert("Your bus will be arriving shortly...");
+  }
 }
 
 window.onload = initPage;
