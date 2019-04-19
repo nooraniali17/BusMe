f2b16f56ad8d306578b743a1643154b0437d8910
Shane Duan <s_duan@u.pacific.edu>
2019-04-19 16:05:46 -0700

    Add better localhost fallback checking

--- express_server/server.js
+++ express_server/server.js
@@ -1,12 +1,18 @@
 const config = require("config");
 const app = require("./app");
 
+let greenlockConfig;
 try {
+  greenlockConfig = config.get("greenlock");
+} catch (e) { }
+
+if (greenlockConfig) {
   require("greenlock-express").create({
-    ...config.get("greenlock"),
+    ...greenlockConfig,
+    store: require("greenlock-store-fs"),
     app,
   }).listen(80, 443, () => console.log("listening on public (80/443)."));
-} catch (e) {
+} else {
   const port = config.get("port");
-  app.listen(port, () => console.log("listening on port", port))
+  app.listen(port, () => console.log("listening on port", port));
 }
