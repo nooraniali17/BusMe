d3f3c10dfd75a05dc9865195b86d6827b27d8610
Gurkirt Heerey <g_heerey@u.pacific.edu>
2019-04-20 22:27:50 -0700

    Initialized

--- BusMe_DB.sql
+++ BusMe_DB.sql
@@ -3,7 +3,15 @@ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password
 ALTER USER 'admin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 
 delete from pass_info where stop_name = "Calvary First Church";
-
+ 
 select * from pass_info;
 
-delete from pass_info where stop_name = "Walnut Ave & Eureka St";
+delete from pass_info where stop_name = "N. First St. & Walnut Ave.";
+
+SELECT EXISTS(SELECT * FROM pass_info WHERE stop_name = "Alpine & Grange Ave EB");
+
+update pass_info 
+SET num_pass = 9
+WHERE stop_name = "W. Las Palmas Ave. & Ward Ave."
+
+
