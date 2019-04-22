23af65b0295edc9d062a60a7a5771646d6922c85
Shane Duan <s_duan@u.pacific.edu>
2019-04-22 15:02:47 -0700

    Enforce semistandard style

--- express_server/app.js
+++ express_server/app.js
@@ -1,19 +1,19 @@
-const bodyParser = require("body-parser");
-const config = require("config");
-const cors = require("cors");
-const express = require("express");
-const morgan = require("morgan");
-const schedule = require("node-schedule");
-const serveStatic = require("serve-static");
-const sqlite = require("sqlite");
-const SQL = require("sql-template-strings");
+const bodyParser = require('body-parser');
+const config = require('config');
+const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const schedule = require('node-schedule');
+const serveStatic = require('serve-static');
+const sqlite = require('sqlite');
+const SQL = require('sql-template-strings');
 
 /**
  * Wrap middleware function with default error handling, so that there is no
  * need to add try-catch blocks manually.
- * 
+ *
  * Equivalent of:
- * 
+ *
  * ```js
  * try {
  *  await fn(...args);
@@ -22,10 +22,10 @@ const SQL = require("sql-template-strings");
  *  res.sendStatus(500);
  * }
  * ```
- * 
+ *
  * @param fn Async function to wrap.
  */
-function asyncCatch(fn) {
+function asyncCatch (fn) {
   return (req, res, ...args) => Promise.resolve(fn(req, res, ...args))
     .catch(err => {
       console.error(err);
@@ -33,22 +33,22 @@ function asyncCatch(fn) {
     });
 }
 
-if (!config.has("db")) {
-  throw new Error("Please specify a file to use as sqlite database.");
+if (!config.has('db')) {
+  throw new Error('Please specify a file to use as sqlite database.');
 }
 
 const db = (async () => {
-  const _ = await sqlite.open(config.get("db"));
-  // execute migration script in `migrations/*`, in our case it's just to 
+  const _ = await sqlite.open(config.get('db'));
+  // execute migration script in `migrations/*`, in our case it's just to
   // initialize the database in general.
-  await _.migrate({ force: "last" });
-  return _
+  await _.migrate({ force: 'last' });
+  return _;
 })();
 
 const app = express();
 
 // logging (e.g. `::ffff:127.0.0.1 - GET / HTTP/1.1 304 - - 9.584 ms`)
-app.use(morgan("short"));
+app.use(morgan('short'));
 
 // https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
 app.use(cors());
@@ -57,17 +57,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // get all current checkins (table dump)
-app.get("/api/checkin", asyncCatch(async (req, res) => {
-  res.send(await (await db).all("select * from pass_info"));
+app.get('/api/checkin', asyncCatch(async (req, res) => {
+  res.send(await (await db).all('select * from pass_info'));
 }));
 
 /**
  * Get passenger count at a stop.
- * 
+ *
  * @param stop_name Google maps query of which stop to get count for.
  * @returns Number of passengers, or `undefined` if the stop doesn't exist.
  */
-async function getPassengerCountAt(stop_name) {
+async function getPassengerCountAt (stop_name) {
   const data = await (await db).get(SQL`
     select num_pass from pass_info
     where
@@ -83,7 +83,7 @@ async function getPassengerCountAt(stop_name) {
 /**
  * Set number of passengers at `stop_name` to `num_pass` passengers.
  */
-async function setPassengerCount(stop_name, num_pass) {
+async function setPassengerCount (stop_name, num_pass) {
   return (await db).run(SQL`
     update pass_info
       set num_pass = ${num_pass}
@@ -93,15 +93,15 @@ async function setPassengerCount(stop_name, num_pass) {
 
 /**
  * Pick user up.
- * 
+ *
  * schema: dict:
  *  picked_up: int: number of passengers that got picked up.
  *  stop_name: str: google maps query for the stop.
  */
-app.post("/api/pickup", asyncCatch(async ({ body = {} }, res) => {
+app.post('/api/pickup', asyncCatch(async ({ body = {} }, res) => {
   const { picked_up, stop_name } = body;
   if (!picked_up || !stop_name) {
-    console.log("not all params are filled;", picked_up, stop_name);
+    console.log('not all params are filled;', picked_up, stop_name);
     return res.sendStatus(400);
   }
 
@@ -121,32 +121,32 @@ app.post("/api/pickup", asyncCatch(async ({ body = {} }, res) => {
 
 /**
  * Add new checkin.
- * 
+ *
  * schema: dict:
  *  num_pass: int: number of passengers (1..10).
  *  latitude, longitude: float:
  *    geolocation of stop (should be handled by gmaps).
  *  stop_name: str: google maps query for the stop.
- * 
+ *
  * returns: int: id for future reference in POST /api/checkin/cancel
  */
-app.post("/api/checkin", asyncCatch(async ({ body = {} }, res) => {
+app.post('/api/checkin', asyncCatch(async ({ body = {} }, res) => {
   const params = [
-    "num_pass",
-    "latitude",
-    "longitude",
-    "stop_name"
+    'num_pass',
+    'latitude',
+    'longitude',
+    'stop_name'
   ].map(k => body[k]);
 
   if (!params.every(v => v)) {
-    console.log("not all params are filled;", params);
+    console.log('not all params are filled;', params);
     return res.sendStatus(400);
   }
 
   const [pass, lat, lng, stop] = params;
 
   if (pass <= 0 || pass > 10) {
-    console.log("passenger count not between 1 and 10, got", pass, "instead");
+    console.log('passenger count not between 1 and 10, got', pass, 'instead');
     return res.sendStatus(400);
   }
 
@@ -165,15 +165,15 @@ app.post("/api/checkin", asyncCatch(async ({ body = {} }, res) => {
 
 /**
  * Cancel checkin.
- * 
+ *
  * schema: dict:
  *  id: int:
  *    ID to delete (should be kept from the return result of POST /api/checkin)
  */
-app.post("/api/checkin/cancel", asyncCatch(async ({ body = {} }, res) => {
+app.post('/api/checkin/cancel', asyncCatch(async ({ body = {} }, res) => {
   const { stop_name, num_pass } = body;
   if (!stop_name || !num_pass) {
-    console.log("no stop name or passenger count provided, instead got", body);
+    console.log('no stop name or passenger count provided, instead got', body);
     return res.sendStatus(400);
   }
 
@@ -185,12 +185,12 @@ app.post("/api/checkin/cancel", asyncCatch(async ({ body = {} }, res) => {
 }));
 
 // proxy static files to avoid multi origin trouble during local development
-app.use(serveStatic("../js_client", { index: "passenger.html" }));
+app.use(serveStatic('../js_client', { index: 'passenger.html' }));
 
 module.exports = app;
 
 // and schedule a reset of all stops every day at midnight
 const job = schedule.scheduleJob({ hour: 0, minute: 0 }, async () => {
-  console.log("resetting stored stops.");
-  (await db).run("delete from pass_info");
+  console.log('resetting stored stops.');
+  (await db).run('delete from pass_info');
 });
--- express_server/server.js
+++ express_server/server.js
@@ -1,20 +1,20 @@
-const config = require("config");
-const app = require("./app");
+const config = require('config');
+const app = require('./app');
 
 let greenlockConfig;
 try {
-  greenlockConfig = config.get("greenlock");
+  greenlockConfig = config.get('greenlock');
 } catch (e) { }
 
 // if greenlock config could not be retrieved or it is not an object,
 // then we use it the regular way.
 if (greenlockConfig) {
-  require("greenlock-express").create({
+  require('greenlock-express').create({
     ...greenlockConfig,
-    store: require("greenlock-store-fs"),
-    app,
-  }).listen(80, 443, () => console.log("listening on public (80/443)."));
+    store: require('greenlock-store-fs'),
+    app
+  }).listen(80, 443, () => console.log('listening on public (80/443).'));
 } else {
-  const port = config.get("port");
-  app.listen(port, () => console.log("listening on port", port));
+  const port = config.get('port');
+  app.listen(port, () => console.log('listening on port', port));
 }
