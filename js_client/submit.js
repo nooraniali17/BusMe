143013e9293fc2ce5e422b5501d534edca7cb05b
Gurkirt Heerey <g_heerey@u.pacific.edu>
2019-04-27 18:03:13 -0700

    Removing checkin complete

--- js_client/submit.js
+++ js_client/submit.js
@@ -4,8 +4,7 @@ let token;
 
 function initPage() {
   tripInfo = JSON.parse(localStorage.getItem("tripInfo"));
-  token = JSON.parse(localStorage.getItem("token"));
-  debugger;
+  token = localStorage.getItem("token");
   console.log(token);
 
   document.getElementById("busStopLabel").innerHTML = tripInfo.stop_name;
@@ -20,7 +19,7 @@ function cancelRequest() {
 
     fetch("/api/checkin/cancel", {
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ token }),
+      body: token,
       method: "POST"
     })
       .then((...data) => {
