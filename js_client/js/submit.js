5be1c2302a83496b715b1f3712ede39180869e91
Jesse Talamantes <j_talamantes@u.pacific.edu>
2019-04-30 11:32:18 -0700

    Add function to show driver location to the passenger

--- js_client/js/submit.js
+++ js_client/js/submit.js
@@ -1,4 +1,7 @@
-import { getStopInfo, getStopName } from './impl/map.js';
+import { getStopInfo, getStopName, initMap } from './impl/map.js';
+
+let map;
+let infoWindow;
 
 const payload = {
   headers: { 'Content-Type': 'application/json' },
@@ -20,6 +23,45 @@ function setTable (data) {
   $('#pass').text(passengers);
   $('#stop').text(stopName);
 }
+/**
+ * Add markers of all nearby bus stations.
+ * 
+ * @param location LatLng literal to base the query on.
+ * @param radius How far away the query should look for.
+ */
+async function addDriverMarker (location, radius) {
+  var driverLatLng = {lat: 37.970843, lng: -121.315699}
+
+  var driverMaker =new google.maps.Marker({
+    position: driverLatLng,
+    title:"Here's your driver!"
+  });
+  // const res = await gmapsTextSearch(
+  //   new google.maps.places.PlacesService(map),
+  //   { location, radius, query: 'bus stops' }
+  // );
+
+  // const animation = google.maps.Animation.DROP;
+  // for (const r of res) {
+  //   const { name, place_id, geometry: { location } } = r;
+  //   const position = { lat: location.lat(), lng: location.lng() };
+  //   new google.maps.Marker({ map, position, animation })
+  //     .addListener('click', function () {
+  //       infoWindow.setContent(name);
+  //       infoWindow.open(map, this);
+  //       chosenLocation = r;
+  //     });
+  // }
+}
+
+$(document).ready(async () => {
+  $('input').tooltip({ trigger: 'focus' });
+
+  const mapData = await initMap();
+  map = mapData.map;
+  infoWindow = mapData.infoWindow;
+  await addDriverMarker(map.getCenter(), 50);
+});
 
 (async () => {
   try {
